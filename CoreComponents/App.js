99ac3a15dbb77ad6b293b62f5eff576dd79d820c
James Kwon <jhk1097@gmail.com>
2024-05-11 19:12:42 +1200

    Pressable learnt, onPress, onPressIn, onPressOut all working

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,5 +1,5 @@
-import { View, Button } from 'react-native'
-// const logoImg = require('./assets/adaptive-icon.png')
+import { View, Button, Image, Text, Pressable } from 'react-native'
+const logoImg = require('./assets/adaptive-icon.png')
 
 export default function App() {
   return (
@@ -8,8 +8,26 @@ export default function App() {
         title="Press"
         onPress={() => console.log('Button pressed')}
         color="midnightblue"
-        disabled
-      ></Button>
+      />
+      <Pressable onPress={() => console.log('Image pressed out')}>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      </Pressable>
+      <Pressable onPress={() => console.log('Text pressed')}>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. In hac
+          habitasse platea dictumst quisque sagittis purus sit amet. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Et
+          netus et malesuada fames ac turpis egestas sed. Non blandit massa enim
+          nec dui nunc. Cursus sit amet dictum sit amet justo donec. Dignissim
+          diam quis enim lobortis scelerisque fermentum dui faucibus. Amet justo
+          donec enim diam. Habitasse platea dictumst quisque sagittis. Vel risus
+          commodo viverra maecenas. Pharetra diam sit amet nisl suscipit
+          adipiscing bibendum est. Bibendum at varius vel pharetra vel turpis
+          nunc eget. Arcu odio ut sem nulla pharetra diam sit. Orci a
+          scelerisque purus semper eget duis.
+        </Text>
+      </Pressable>
     </View>
   )
 }
