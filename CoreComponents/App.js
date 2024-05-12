f9ae7c09352ad64f6d73dd4cf50a56f2f1bf8ba2
James Kwon <jhk1097@gmail.com>
2024-05-12 23:49:29 +1200

    Adding activity indicator

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,20 +1,32 @@
-import { useState } from 'react'
-import { View, StatusBar, Button } from 'react-native'
+// import { useState } from 'react'
+// import { View, StatusBar, Button } from 'react-native'
+
+// export default function App() {
+//   const [isStatusVisible, setIsStatusVisible] = useState(false)
+//   return (
+//     <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
+//       <Button
+//         title="Hide / Show StatusBar"
+//         onPress={() => setIsStatusVisible(!isStatusVisible)}
+//       />
+
+//       <StatusBar
+//         backgroundColor="lightgreen"
+//         barStyle="light-content"
+//         hidden={isStatusVisible}
+//       />
+//     </View>
+//   )
+// }
+
+import { View, ActivityIndicator } from 'react-native'
 
 export default function App() {
-  const [isStatusVisible, setIsStatusVisible] = useState(false)
   return (
     <View style={{ flex: 1, backgroundColor: 'plum', padding: 60 }}>
-      <Button
-        title="Hide / Show StatusBar"
-        onPress={() => setIsStatusVisible(!isStatusVisible)}
-      />
-
-      <StatusBar
-        backgroundColor="lightgreen"
-        barStyle="light-content"
-        hidden={isStatusVisible}
-      />
+      <ActivityIndicator />
+      <ActivityIndicator size="small" />
+      <ActivityIndicator size="large" color="midnightblue" animating={false} />
     </View>
   )
 }
